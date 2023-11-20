3b1d597d477ad3c4aaa440b2a7fd7b2a266a85a6
Yash Bhisikar <101914076+yashrb24@users.noreply.github.com>
2023-11-20 21:12:53 +0530

    Update custom_malloc.c - sidk version

--- code-orig/custom_malloc.c
+++ code-orig/custom_malloc.c
@@ -6,8 +6,9 @@
 char globalBuffer[BUFFER_SIZE];
 
 // Define a structure to represent a memory allocation
-typedef struct {
-    void* startAddress;
+typedef struct
+{
+    void *startAddress;
     size_t size;
 } MemoryAllocation;
 
@@ -15,49 +16,45 @@ typedef struct {
 MemoryAllocation allocatedRegions[BUFFER_SIZE];
 size_t numAllocatedRegions = 0;
 
-// Variable to keep track of the current allocation position
 size_t allocationPosition = 0;
 
 // Function to allocate memory from the global buffer
-void* globalMalloc(size_t size) {
+void *globalMalloc(size_t size)
+{
+    // Symbolic allocation size
+    klee_make_symbolic(&size, sizeof(size), "size");
+
     // Check if there is enough space in the buffer
-    if ((allocationPosition + size) <= BUFFER_SIZE) {
-        // Check if the requested memory overlaps with any existing allocations
-        for (size_t i = 0; i <= numAllocatedRegions; ++i) {
-            if (
-                (allocationPosition >= (size_t)allocatedRegions[i].startAddress + allocatedRegions[i].size) ||
-                ((allocationPosition + size) <= (size_t)allocatedRegions[i].startAddress)
-            ) {
-                // No overlap, it's safe to allocate in this region
-                // Calculate the starting address of the allocated memory
-                void* allocatedMemory = (void*)&globalBuffer[allocationPosition];
+    if ((allocationPosition + size) <= BUFFER_SIZE)
+    {
+        // Calculate the starting address of the allocated memory
+        void *allocatedMemory = (void *)&globalBuffer[allocationPosition];
 
-                // Store information about the allocated region
-                allocatedRegions[numAllocatedRegions].startAddress = allocatedMemory;
-                allocatedRegions[numAllocatedRegions].size = size;
-                numAllocatedRegions++;
+        // Store information about the allocated region
+        allocatedRegions[numAllocatedRegions].startAddress = allocatedMemory;
+        allocatedRegions[numAllocatedRegions].size = size;
+        numAllocatedRegions++;
 
-                // Update the allocation position
-                allocationPosition += size;
+        // Update the allocation position
+        allocationPosition += size;
 
-                return allocatedMemory;
-            }
-        }
-
-        // If the loop completes without finding an appropriate region, allocation failed
-        fprintf(stderr, "Error: Unable to find a suitable region for allocation.\n");
-        return NULL;
-    } else {
+        return allocatedMemory;
+    }
+    else
+    {
         // Allocation failed, return NULL or handle the error as needed
         return NULL;
     }
 }
 
 // Function to free allocated memory
-void globalFree(void* ptr) {
+void globalFree(void *ptr)
+{
     // Find the corresponding allocated region
-    for (size_t i = 0; i < numAllocatedRegions; ++i) {
-        if (allocatedRegions[i].startAddress == ptr) {
+    for (size_t i = 0; i < numAllocatedRegions; ++i)
+    {
+        if (allocatedRegions[i].startAddress == ptr)
+        {
             // Clear the allocated region
             allocatedRegions[i].startAddress = NULL;
             allocatedRegions[i].size = 0;
@@ -74,36 +71,30 @@ void globalFree(void* ptr) {
     fprintf(stderr, "Error: Attempting to free unallocated memory.\n");
 }
 
-int main() {
-    // Example usage
-    int* dynamicArray1 = (int*)globalMalloc(5 * sizeof(int));
-    int* dynamicArray2 = (int*)globalMalloc(3 * sizeof(int));
+int main()
+{
+    int *dynamicArray1 = (int *)globalMalloc(5 * sizeof(int));
+    int *dynamicArray2 = (int *)globalMalloc(3 * sizeof(int));
 
-    // Use the allocated memory
-    for (int i = 0; i < 5; ++i) {
-        dynamicArray1[i] = i * 2;
-    }
-
-    for (int i = 0; i < 3; ++i) {
-        dynamicArray2[i] = i * 3;
-    }
+    // Symbolic array values
+    klee_make_symbolic(dynamicArray1, 5 * sizeof(int), "dynamicArray1");
+    klee_make_symbolic(dynamicArray2, 3 * sizeof(int), "dynamicArray2");
 
     // Print the values
-    for (int i = 0; i < 5; ++i) {
+    for (int i = 0; i < 5; ++i)
+    {
         printf("%d ", dynamicArray1[i]);
     }
 
     printf("\n");
 
-    for (int i = 0; i < 3; ++i) {
+    for (int i = 0; i < 3; ++i)
+    {
         printf("%d ", dynamicArray2[i]);
     }
 
-    // Free allocated memory
     globalFree(dynamicArray1);
     globalFree(dynamicArray2);
-
-    // No need to free; global buffer persists throughout the program's execution
 
     return 0;
 }
